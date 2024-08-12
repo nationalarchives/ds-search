6f39e2118000643ca47701aef30a1ec43944974a
Andrew Hosgood <mail@andrewhosgood.com>
2024-08-12 20:21:02 +0100

    Reset STATIC_ROOT and STATICFILES_DIRS

--- config/settings/base.py
+++ config/settings/base.py
@@ -111,8 +111,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "app/static")
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, "app/static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "app/static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
