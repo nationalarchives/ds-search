ae50addf50fbc18c5f50b1666d48c392c00a30ce
TNA-Allan <allan.fernandes@nationalarchives.gov.uk>
2025-01-10 14:37:04 +0000

    FIND-81:add logging config

--- config/settings/base.py
+++ config/settings/base.py
@@ -237,3 +237,18 @@ CLIENT_KEY = os.getenv("ROSETTA_CLIENT_KEY")
 CLIENT_VERIFY_CERTIFICATES = strtobool(
     os.getenv("ROSETTA_CLIENT_VERIFY_CERTIFICATES", "True")
 )
+
+# logging
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+        "level": "WARNING",
+    },
+}
