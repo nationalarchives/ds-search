d803ddb12063ae450d7a1d9ed9b4169609b4ea23
TNA-Allan <allan.fernandes@nationalarchives.gov.uk>
2025-01-20 14:21:47 +0000

    FIND-81:SENTRY_SAMPLE_RATE amends

--- config/settings/sentry.py
+++ config/settings/sentry.py
@@ -12,10 +12,9 @@ if SENTRY_DSN := os.getenv("SENTRY_DSN", ""):
         environment=os.getenv("ENVIRONMENT_NAME", "production"),
         release=get_git_sha(),
         integrations=[DjangoIntegration()],
-        # Set traces_sample_rate to 1.0 to capture 100%
-        # of transactions for performance monitoring.
-        # We recommend adjusting this value in production.
+        sample_rate=settings.SENTRY_SAMPLE_RATE,
         traces_sample_rate=settings.SENTRY_SAMPLE_RATE,
+        profiles_sample_rate=settings.SENTRY_SAMPLE_RATE,
         # If you wish to associate users to errors (assuming you are using
         # django.contrib.auth) you may enable sending PII data.
         send_default_pii=strtobool(os.getenv("SENTRY_SEND_USER_DATA", "False")),
