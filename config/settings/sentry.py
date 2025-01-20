c3d4ac3e9387ded3646f0c7876ff684b8540b9ba
TNA-Allan <allan.fernandes@nationalarchives.gov.uk>
2025-01-20 14:23:40 +0000

    FIND-81:SENTRY_SEND_USER_DATA amends

--- config/settings/sentry.py
+++ config/settings/sentry.py
@@ -15,7 +15,4 @@ if SENTRY_DSN := os.getenv("SENTRY_DSN", ""):
         sample_rate=settings.SENTRY_SAMPLE_RATE,
         traces_sample_rate=settings.SENTRY_SAMPLE_RATE,
         profiles_sample_rate=settings.SENTRY_SAMPLE_RATE,
-        # If you wish to associate users to errors (assuming you are using
-        # django.contrib.auth) you may enable sending PII data.
-        send_default_pii=strtobool(os.getenv("SENTRY_SEND_USER_DATA", "False")),
     )
