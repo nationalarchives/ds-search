bfefa2a41cd98798d3f6dc95e6deaf4fed7a9f60
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-02-03 13:00:47 +0000

    Fix Sentry release
    https://docs.sentry.io/platforms/python/configuration/releases/#setting-a-release

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,11 @@ if settings.SENTRY_DSN:
     sentry_sdk.init(
         dsn=settings.SENTRY_DSN,
         environment=settings.ENVIRONMENT_NAME,
-        release=settings.BUILD_VERSION,
+        release=(
+            f"ds-search@{settings.BUILD_VERSION}"
+            if settings.BUILD_VERSION
+            else ""
+        ),
         integrations=[DjangoIntegration()],
         sample_rate=settings.SENTRY_SAMPLE_RATE,
         traces_sample_rate=settings.SENTRY_SAMPLE_RATE,
