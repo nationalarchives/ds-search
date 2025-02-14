f34605973588850095d65912d4ec9f7c75b53024
TNA-Allan <allan.fernandes@nationalarchives.gov.uk>
2025-02-14 13:25:42 +0000

    restore id converter, tests

--- test/records/test_id_formats.py
+++ test/records/test_id_formats.py
@@ -10,9 +10,6 @@ class TestIDFormats(SimpleTestCase):
         for label, value in (
             ("longformat", "3717ee38900740728076a61a398fcb84"),
             ("guid", "4d8dae2c-b417-4614-8ed8-924b9b4beeac"),
-            ("guid-ish 1", "c5f5-0f02-4d03-9b5c-8ec7973794b7"),
-            ("guid-ish 2", "d7b-e1c2-45b2-83dc-17df01b13e7e"),
-            ("guid-ish 3", "f0-dab7f878b99b"),
             ("dri_guid_plus", "00149557ca64456a8a41e44f14621801_1"),
             ("iaid_A", "A13530124"),
             ("iaid_C", "C2341693"),
