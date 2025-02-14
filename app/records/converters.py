13c6ad4c1a6d3dbfa9e6ad237e5cb0c767662e15
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-02-14 10:45:20 +0000

    Catch more invalid GUIDs

--- app/records/converters.py
+++ app/records/converters.py
@@ -6,4 +6,4 @@ class IDConverter(StringConverter):
 
     # This should be the correct regex but some of the GUIDs are not valid
     # regex = r"([ACDFN][0-9]{1,8}|[a-f0-9]{8}-?([a-f0-9]{4}-?){3}[a-f0-9]{12}(_[1-9])?)"
-    regex = r"([ACDFN][0-9]{1,8}|[a-f0-9]{4,8}-?([a-f0-9]{4}-?){3}[a-f0-9]{12}(_[1-9])?)"
+    regex = r"([ACDFN][0-9]{1,8}|([a-f0-9]{4,8}-?)?([a-f0-9]{2,4}-?){1,4}[a-f0-9]{12}(_[1-9])?)"
