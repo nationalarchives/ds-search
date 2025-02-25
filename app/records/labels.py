18e901fe3ddca08d2aaf412b08a17d80e3eb5a63
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-02-25 12:21:51 +0000

    Alphabetise FIELD_LABELS

--- app/records/labels.py
+++ app/records/labels.py
@@ -1,21 +1,38 @@
 FIELD_LABELS = {
-    "reference_number": {
-        "title": "Reference",
-        "description": "A unique code used to identify and order the record",
+    "access_condition": {"title": "Access conditions"},
+    "accumulation_dates": {"title": "Accumulation dates"},
+    "accruals": {"title": "Accruals"},
+    "administrative_background": {
+        "title": "Administrative / biographical background"
     },
-    "title": {"title": "Title"},
-    "summary_title": {"title": "Summary title"},
+    "appraisal_information": {"title": "Selection and destruction information"},
+    "arrangement": {"title": "Arrangement"},
+    "closure_status": {
+        "title": "Closure status",
+        "description": "If the record is 'open' or 'closed' for public access",
+    },
+    "copies_information": {"title": "Copies information"},
+    "creator": {"title": "Creator"},
+    "custodial_history": {"title": "Custodial history"},
     "date_covering": {
         "title": "Date",
         "description": "When the record was created",
     },
-    "creator": {"title": "Creator"},
+    "description": {
+        "title": "Description",
+        "description": "What the record is about",
+    },
     "dimensions": {"title": "Dimensions"},
     "former_department_reference": {
         "title": "Former reference in its original department"
     },
     "former_pro_reference": {
         "title": "Former reference in The National Archives"
+    },
+    "held_by": {"title": "Held by", "description": "Who holds the record"},
+    "hierarchy": {"title": "Where am I in the catalogue?"},
+    "immediate_source_of_acquisition": {
+        "title": "Immediate source of acquisition"
     },
     "language": {
         "title": "Language",
@@ -25,44 +42,26 @@ FIELD_LABELS = {
         "title": "Legal status",
         "description": "If the record is available to the public",
     },
+    "location_of_originals": {"title": "Originals held at"},
     "map_designation": {"title": "Map designation"},
     "map_scale": {"title": "Map scale"},
+    "next": {"title": "Next"},
     "note": {"title": "Note"},
+    "parent": {"title": "This record is in"},
     "physical_condition": {"title": "Physical condition"},
     "physical_description": {"title": "Physical description"},
-    "held_by": {"title": "Held by", "description": "Who holds the record"},
-    "access_condition": {"title": "Access conditions"},
-    "closure_status": {
-        "title": "Closure status",
-        "description": "If the record is 'open' or 'closed' for public access",
-    },
-    "record_opening": {"title": "Record opening date"},
-    "accruals": {"title": "Accruals"},
-    "accumulation_dates": {"title": "Accumulation dates"},
-    "appraisal_information": {"title": "Selection and destruction information"},
-    "copies_information": {"title": "Copies information"},
-    "custodial_history": {"title": "Custodial history"},
-    "immediate_source_of_acquisition": {
-        "title": "Immediate source of acquisition"
-    },
-    "location_of_originals": {"title": "Originals held at"},
-    "restrictions_on_use": {"title": "Restrictions on use"},
-    "administrative_background": {
-        "title": "Administrative / biographical background"
-    },
-    "arrangement": {"title": "Arrangement"},
-    "publication_note": {"title": "Publication note"},
-    "related_materials": {"title": "Related material"},
-    "description": {
-        "title": "Description",
-        "description": "What the record is about",
-    },
-    "separated_materials": {"title": "Separated material"},
-    "unpublished_finding_aids": {"title": "Unpublished finding aids"},
-    "hierarchy": {"title": "Where am I in the catalogue?"},
-    "next": {"title": "Next"},
     "previous": {"title": "Previous"},
-    "parent": {"title": "This record is in"},
-    "related_records": {"title": "Related records"},
+    "publication_note": {"title": "Publication note"},
+    "record_opening": {"title": "Record opening date"},
+    "reference_number": {
+        "title": "Reference",
+        "description": "A unique code used to identify and order the record",
+    },
     "related_articles": {"title": "Related content"},
+    "related_materials": {"title": "Related material"},
+    "related_records": {"title": "Related records"},
+    "restrictions_on_use": {"title": "Restrictions on use"},
+    "separated_materials": {"title": "Separated material"},
+    "summary_title": {"title": "Summary title"},
+    "unpublished_finding_aids": {"title": "Unpublished finding aids"},
 }
