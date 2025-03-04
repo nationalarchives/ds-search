273f8d69c065140982f2a97cd32bd5b62fca2bae
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-03-04 11:57:28 +0000

    Add pagination support in search

--- app/search/views.py
+++ app/search/views.py
@@ -1,3 +1,4 @@
+from app.errors import views as errors_view
 from app.lib.api import ResourceNotFound
 from app.search.api import search_records
 from django.http import HttpResponse
@@ -6,12 +7,29 @@ from django.template import loader
 
 def catalogue_search_view(request):
     template = loader.get_template("search/catalogue.html")
+    results_per_page = 20
+    page = int(request.GET.get("page", 1))
     try:
-        results = search_records(request.GET.get("q", None))
+        results = search_records(
+            query=request.GET.get("q", None),
+            results_per_page=results_per_page,
+            page=page,
+        )
     except ResourceNotFound:
         return HttpResponse(template.render({}, request))
+    results_range = {
+        "from": ((page - 1) * results_per_page) + 1,
+        "to": ((page - 1) * results_per_page) + results.stats_results,
+    }
+    print(results_range["to"])
+    print(results.stats_total)
+    if results_range["to"] > results.stats_total:
+        return errors_view.page_not_found_error_view(request=request)
     context = {
         "results": results.records,
+        "results_per_page": results_per_page,
+        "page": page,
+        "results_range": results_range,
         "stats": {
             "total": results.stats_total,
             "results": results.stats_results,
