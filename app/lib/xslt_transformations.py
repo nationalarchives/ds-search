b4670c2e367a697477251a8ff20e7fa36d31b35b
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-03-26 11:53:44 +0000

    Add ignore schemas list to XSLT transformations

--- app/lib/xslt_transformations.py
+++ app/lib/xslt_transformations.py
@@ -6,10 +6,56 @@ SCHEMAS = {
     "RoyalMarines": "app/resources/xslt/RoyalMarines_DetailScope_inc.xsl"
 }
 
+IGNORE_SCHEMAS = [
+    "default",
+    "Airwomen",
+    "AliensRegCards",
+    "AncientPetitions",
+    "BritishWarMedal",
+    "CabinetPapers",
+    "CombatRepWW2",
+    "DNPC",
+    "DeathDuty",
+    "DomesdayBook",
+    "EffectsPapers",
+    "FameWill",
+    "GallantrySea",
+    "LootedArt",
+    "Medal",
+    "Miscellaneous",
+    "MusterRolls",
+    "NavalOfficers",
+    "NavalReserve",
+    "NavyLandService",
+    "NursingService",
+    "Olympic",
+    "PoorLaw",
+    "prisoner",
+    "PrisonerInterview",
+    "RAFOfficers",
+    "RNASOfficers",
+    "RNOfficer",
+    "RecHonours",
+    "SeamenMedal",
+    "SeamenRegister",
+    "SeamenWill",
+    "ShippingSeamen",
+    "Squadron",
+    "Titanic",
+    "VictoriaCross",
+    "VolunteerReserve",
+    "Will",
+    "WomensCorps",
+    "Wrns",
+]
+
+
 logger = logging.getLogger(__name__)
 
 
 def apply_xslt(html_source, schema):
+    if schema in IGNORE_SCHEMAS:
+        return html_source
     dom = html.fromstring(html_source)
     try:
         schema_xslt = SCHEMAS[schema]
