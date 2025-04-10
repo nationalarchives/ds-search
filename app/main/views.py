4897b6ea5883b79230a3dd5f54527f97cb0606f9
James Biggs <james.biggs@nationalarchives.gov.uk>
2025-04-10 17:04:06 +0100

    Make basic request for latest 3 EtC content and stories, time period, and topic listing pages

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.template import loader
-
+from app.lib.api import JSONAPIClient
+from django.conf import settings
 
 def index(request):
     template = loader.get_template("main/index.html")
@@ -10,7 +11,17 @@ def index(request):
 
 def catalogue(request):
     template = loader.get_template("main/catalogue.html")
-    context = {"foo": "bar"}
+    context = {}
+
+    client = JSONAPIClient(settings.ETNA_API_URL)
+    client.add_parameters({"child_of": 55, "limit": 3, "order": "-first_published_at"})
+    response_data = client.get("/pages/")
+    context['pages'] = response_data.get("items", [])
+
+    client.add_parameters({"child_of": 5, "limit": 3, "order": "-first_published_at"})
+    response_data = client.get("/pages/")
+    context['top_pages'] = response_data.get("items", [])
+
     return HttpResponse(template.render(context, request))
 
 
