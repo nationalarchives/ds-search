79cb7f9b2ceb0a04cce22142bfe100d3cd96b821
TNA-Allan <allan.fernandes@nationalarchives.gov.uk>
2025-06-12 10:26:14 +0100

    FIND-263:convert http codes to use from HTTPStatus

--- app/errors/views.py
+++ app/errors/views.py
@@ -1,13 +1,15 @@
+from http import HTTPStatus
+
 from django.shortcuts import render
 
 
 def page_not_found_error_view(request, exception=None):
     response = render(request, "errors/page_not_found.html")
-    response.status_code = 404
+    response.status_code = HTTPStatus.NOT_FOUND
     return response
 
 
 def server_error_view(request, exception=None):
     response = render(request, "errors/server_error.html")
-    response.status_code = 500
+    response.status_code = HTTPStatus.INTERNAL_SERVER_ERROR
     return response
