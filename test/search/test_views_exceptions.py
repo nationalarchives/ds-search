7669e27da80d5433db48116c6fe1d2d657dd9663
TNA-Allan <allan.fernandes@nationalarchives.gov.uk>
2025-06-12 11:34:40 +0100

    FIND-263:added test case

---
+++ test/search/test_views_exceptions.py
@@ -0,0 +1,25 @@
+import responses
+from django.conf import settings
+from django.test import TestCase, override_settings
+from http import HTTPStatus
+
+class TestCatalogeSearchViewExceptions(TestCase):
+
+    @override_settings(
+        ROSETTA_API_URL="",
+    )
+    @responses.activate
+    def test_missing_config_with_server_error(self):
+        responses.add(
+            responses.GET,
+            f"{settings.ROSETTA_API_URL}/search",
+            status=HTTPStatus.OK,
+        )
+
+        with self.assertLogs("app.search.views", level="ERROR") as lc:
+            response = self.client.get("/catalogue/search/")
+
+        self.assertEqual(response.status_code, HTTPStatus.INTERNAL_SERVER_ERROR)
+        self.assertIn(
+            "ERROR:app.search.views:ROSETTA_API_URL not set", lc.output
+        )
