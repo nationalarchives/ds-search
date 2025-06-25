e812f74992ac52b71a8fbeaeba8fc27660dec63f
TNA-Allan <allan.fernandes@nationalarchives.gov.uk>
2025-06-25 17:14:19 +0100

    FIND-251:added tests for level filter replacement values for UI-API

--- test/search/test_views.py
+++ test/search/test_views.py
@@ -4,6 +4,7 @@ import responses
 from app.records.models import Record
 from app.search.buckets import BucketKeys
 from app.search.forms import CatalogueSearchForm
+from app.search.views import APIMixin
 from django.conf import settings
 from django.test import TestCase
 
@@ -405,5 +406,37 @@ class CatalogueSearchViewTests(TestCase):
                 {"text": "Sub-sub-series", "value": "Sub-sub-series"},
                 {"text": "Piece", "value": "Piece"},
                 {"text": "Item", "value": "Item", "checked": True},
+            ],
+        )
+
+
+class APIMixinReplaceMethodsTests(TestCase):
+
+    def setUp(self):
+        self.apimixin = APIMixin()
+
+    def test_replace_input_data_level_field_name(self):
+
+        self.field_name = "level"
+        self.selected_values = ["Division", "Department"]
+        self.selected_values = self.apimixin.replace_input_data(
+            self.field_name, self.selected_values
+        )
+        self.assertEqual(self.selected_values, ["Division", "Lettercode"])
+
+    def test_replace_api_data_level_field_name(self):
+
+        self.field_name = "level"
+        self.entries_data = [
+            {"value": "Division", "doc_count": 1215},
+            {"value": "Lettercode", "doc_count": 435},
+        ]
+
+        self.apimixin.replace_api_data(self.field_name, self.entries_data)
+        self.assertEqual(
+            self.entries_data,
+            [
+                {"value": "Division", "doc_count": 1215},
+                {"value": "Department", "doc_count": 435},
             ],
         )
