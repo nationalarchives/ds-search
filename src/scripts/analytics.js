ec70a0fe004b2d03e972bbe8fcbc752a886f86a3
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2025-07-30 12:50:31 +0100

    Fix tracking for the record details hierarchy

--- src/scripts/analytics.js
+++ src/scripts/analytics.js
@@ -12,30 +12,31 @@ if (ga4Id) {
       eventName: "double_click",
       on: "dblclick",
       data: {
-        // eslint-disable-next-line no-unused-vars
-        state: ($el, $scope, event, index) => helpers.getXPathTo(event.target),
-        // eslint-disable-next-line no-unused-vars
-        value: ($el, $scope, event, index) => event.target.innerHTML,
+        state: ($el, $scope, event) => helpers.getXPathTo(event.target),
+        value: helpers.valueGetters.html,
       },
     },
   ]);
 
-  analytics.addListeners('.etna-details-hierarchy', 'hierarchy',[
-    {
-      eventName: "select",
-      rootEventName: "select_hierarchy",
-      targetElement: ".analytics-hierarchy-link",
-      on: "click",
-      data: {
-        position: helpers.valueGetters.index,
-        section: ($el) => helpers.getClosestHeading($el),
+  analytics.addListeners(
+    ".etna-details-hierarchy",
+    "hierarchy",
+    [
+      {
+        eventName: "select",
+        targetElement: ".analytics-hierarchy-link",
+        on: "click",
+        data: {
+          group: helpers.valueGetters.closestHeading,
+        },
+        rootData: {
+          data_component_name: "catalogue_hierarchy",
+          data_link: ($el) => $el.dataset.analyticsLevel,
+          data_link_type: "link",
+          data_position: helpers.valueGetters.index,
+        },
       },
-      rootData: {
-        data_component_name: "catalogue_hierarchy",
-        data_link: ($el) => $el.dataset.analyticsLevel,
-        data_link_type: "link",
-        data_position: helpers.valueGetters.index,
-      },
-    }
-  ]);
+    ],
+    "select_hierarchy",
+  );
 }
